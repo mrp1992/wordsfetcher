87c62b2bbbd0bd1babaa373249eb873ba579c1a2
Mohan <mohan_hassan@yahoo.com>
2019-02-08 12:15:34 +0530

    remove unnecessary async from test

--- test/index.test.js
+++ test/index.test.js
@@ -3,21 +3,21 @@ const app = require('../index.js');
 
 describe('Test index', () => {
    
-    test('should return bad request response', async() => {
+    test('should return bad request response', () => {
         return request(app).get("/words.json").then(response => {
             expect(response.status).toBe(400);
             expect(response.body.Message).toEqual('Missing value of stringVar');
         })
     });
 
-    test('should return words as per request', async() => {
+    test('should return words as per request', () => {
         return request(app).get("/words.json?stringVar=saloni").then(response => {
             expect(response.status).toBe(200);
             expect(response.body.words[0]).toEqual('Saloni');
         })
     });
 
-    test('should return no words when no words match the given string', async() => {
+    test('should return no words when no words match the given string', () => {
         return request(app).get("/words.json?stringVar=abc").then(response => {
             expect(response.status).toBe(200);
             expect(response.body.words).toEqual([]);
